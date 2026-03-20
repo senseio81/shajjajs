7a50c33cc155bb777726d25f00b526d1370ca334
senseio81 <mu6441829@gmail.com>
2026-03-20 22:21:54 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import asyncio
 import os
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, Router
 from aiogram.types import Message
 from dotenv import load_dotenv
 
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+EMOJI_ID = "5413879192267805083"
+
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
+router = Router()
+dp.include_router(router)
 
-@dp.message()
+@router.message()
 async def handle(message: Message):
     await message.answer(
-        '<tg-emoji emoji-id="5472288976221010328"> </tg-emoji>',
+        f'<tg-emoji emoji-id="{EMOJI_ID}">🎲</tg-emoji>',
         parse_mode="HTML"
     )
 
