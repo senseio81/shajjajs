0caaec281409dbba9ccdbdfe4136316383dd3827
senseio81 <mu6441829@gmail.com>
2026-03-20 22:10:01 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import asyncio
+import os
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+@dp.message()
+async def handle(message: Message):
+    await message.answer(
+        '<tg-emoji emoji-id="5413879192267805083">🗓</tg-emoji>',
+        parse_mode="HTML",
+    )
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
